f5ba78c0e048e4f5bdfe77e1752549f9283c6861
Alfredo Oliveira <alfredo.coj@gmail.com>
2020-05-13 12:51:19 -0300

    criacao de todas as VMs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # terraform-azure-exemplos
 Exemplos com Terraform na Azure.
+
+
+Referênces: 
+
+https://www.terraform.io/docs/providers/azurerm/r/virtual_machine.html
+
+https://www.terraform.io/docs/providers/azure/r/security_group_rule.html
+
+https://docs.microsoft.com/pt-br/azure/developer/terraform/hub-spoke-on-prem
--- projeto.tf
+++ projeto.tf
@@ -9,10 +9,19 @@ variable "prefix" {
   default = "devops"
 }
 
+
+#############################################################
+################# RESOURCE GROUP ############################
+#############################################################
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-resources"
   location = "West US 2"
 }
+
+
+#############################################################
+################# VIRTUAL NETWORK ###########################
+#############################################################
 
 resource "azurerm_virtual_network" "vn" {
   name                = "${var.prefix}-network"
@@ -21,6 +30,11 @@ resource "azurerm_virtual_network" "vn" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+#############################################################
+################# SUBNETS ###################################
+#############################################################
+
+## SN LOAD BALANCER
 resource "azurerm_subnet" "sn-balanceador" {
   name                 = "${var.prefix}-sn-balanceador"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -28,12 +42,26 @@ resource "azurerm_subnet" "sn-balanceador" {
   address_prefix       = "10.0.1.0/24"
 }
 
+## SN SERVICES
 resource "azurerm_subnet" "sn-services" {
   name                 = "${var.prefix}-sn-services"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
   address_prefix       = "10.0.2.0/24"
 }
+
+## SN MONITORING
+resource "azurerm_subnet" "sn-monitoring" {
+  name                 = "${var.prefix}-sn-monitoring"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefix       = "10.0.3.0/24"
+}
+
+
+#############################################################
+################# PUBLIC IP TO LOAD BALANCER ################
+#############################################################
 
 resource "azurerm_public_ip" "pip-balanceador" {
     name                         = "${var.prefix}-pip-balanceador"
@@ -42,6 +70,12 @@ resource "azurerm_public_ip" "pip-balanceador" {
     allocation_method   = "Dynamic"
 }
 
+
+#############################################################
+################# NETWORK INTERFACES ########################
+#############################################################
+
+## NI LOAD BALANCER
 resource "azurerm_network_interface" "nic-balanceador" {
   name                = "${var.prefix}-nic-balanceador"
   location            = azurerm_resource_group.rg.location
@@ -55,18 +89,50 @@ resource "azurerm_network_interface" "nic-balanceador" {
   }
 }
 
-resource "azurerm_network_interface" "nic-services" {
-  name                = "${var.prefix}-nic-services"
+## NI SERVICE MASTER
+resource "azurerm_network_interface" "nic-services-master" {
+  name                = "${var.prefix}-nic-services-master"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipc-services"
+    name                          = "ipc-services-master"
     subnet_id                     = azurerm_subnet.sn-services.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
+## NI SERVICE NODE
+resource "azurerm_network_interface" "nic-services-node" {
+  name                = "${var.prefix}-nic-services-node"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipc-services-node"
+    subnet_id                     = azurerm_subnet.sn-services.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+## NI MONITORING
+resource "azurerm_network_interface" "nic-monitoring" {
+  name                = "${var.prefix}-nic-monitoring"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipc-monitoring"
+    subnet_id                     = azurerm_subnet.sn-monitoring.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#############################################################
+################# VITUAL MACHINES ###########################
+#############################################################
+
+## Load Balancer
 resource "azurerm_virtual_machine" "vm-balanceador" {
   name                  = "${var.prefix}-vm-balanceador"
   location              = azurerm_resource_group.rg.location
@@ -105,11 +171,12 @@ resource "azurerm_virtual_machine" "vm-balanceador" {
   }
 }
 
+## k8s Master
 resource "azurerm_virtual_machine" "vm-master" {
   name                  = "${var.prefix}-vm-master"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic-services.id]
+  network_interface_ids = [azurerm_network_interface.nic-services-master.id]
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -132,6 +199,86 @@ resource "azurerm_virtual_machine" "vm-master" {
   }
   os_profile {
     computer_name  = "vm-master"
+    admin_username = var.user
+    admin_password = var.password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+## k8s node
+resource "azurerm_virtual_machine" "vm-node" {
+  name                  = "${var.prefix}-vm-node"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic-services-node.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk-node"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "vm-node"
+    admin_username = var.user
+    admin_password = var.password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+
+
+## Monitoring
+resource "azurerm_virtual_machine" "vm-monitoring" {
+  name                  = "${var.prefix}-vm-monitoring"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic-monitoring.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk-monitoring"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "vm-monitoring"
     admin_username = var.user
     admin_password = var.password
   }
