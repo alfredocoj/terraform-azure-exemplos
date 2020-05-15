589e55c0b57ceaff6b4a48c06c4668ffd178065d
Alfredo Oliveira <alfredo.coj@gmail.com>
2020-05-15 14:19:11 -0300

    new conf of haproxy

--- haproxy/push.sh
+++ haproxy/push.sh
@@ -1,3 +1,3 @@
 export IP=13.77.144.158
 scp haproxy/haproxy.cfg root@$IP:/etc/haproxy/haproxy.cfg
-ssh root@$IP "/usr/sbin/haproxy -c -V -f /etc/haproxy/haproxy.cfg && service haproxy restart" 
+ssh root@$IP "/usr/sbin/haproxy -c -V -f /etc/haproxy/haproxy.cfg && service haproxy restart && service haproxy status" 
